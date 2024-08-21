f9977be060892b2444e59a0036a89ed4742ab2a9
ca-terumi-k <kawano.terumi@cloud-ace.jp>
2024-08-21 11:28:34 +0900

    ins drag board

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,12 @@
-import Counter from "@/components/counter";
+// import Counter from "@/components/counter";
+import KanbanBoard from "@/components/board";
+
 export default function Home() {
     return (
         <>
+            <div>{/* <Counter /> */}</div>
             <div>
-                <Counter />
+                <KanbanBoard />
             </div>
         </>
     );
