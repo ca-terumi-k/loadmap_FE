930f71f5bc858740381495abed87b3f14cf84a28
ca-terumi-k <kawano.terumi@cloud-ace.jp>
2024-08-28 08:41:58 +0900

    vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+        VERCEL_URL: process.env.VERCEL_URL,
     },
     images: {
         remotePatterns: [
